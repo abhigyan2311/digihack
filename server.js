2fd915988dc850f0de0edbcd3627087f9322e833
Abhigyan Singh <abhigyan2311@gmail.com>
2018-02-13 20:01:23 +0530

    Added port in Server URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var api = new ParseServer({
   cloud: './cloud.js',
   appId: 'appKey231195',
   masterKey: 'iAmGod231195',
-  serverURL: 'http://ec2-13-127-176-156.ap-south-1.compute.amazonaws.com/parse',
+  serverURL: 'http://ec2-13-127-176-156.ap-south-1.compute.amazonaws.com:1337/parse',
   allowClientClassCreation: true,
   enableAnonymousUsers: false,
   filesAdapter: new S3Adapter(
