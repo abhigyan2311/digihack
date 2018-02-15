d46af8d9f2b6b3df31f09606ce59c7bff6682b96
Abhigyan Singh <abhigyan2311@gmail.com>
2018-02-15 19:21:34 +0000

    Chodu abhigyan

--- cloud.js
+++ cloud.js
@@ -1,20 +1,16 @@
-Parse.Cloud.define("geolocationCoupon", function(request, response) {
 var googleMapsClient = require('@google/maps').createClient({
-          key: 'AIzaSyDSVzX5h_PsWX6llQXnNLvGQv5yHAgdNbQ'
-        });
-
-
-
-googleMapsClient.places({
-language: 'en',
-location: [-33.8670522,151.1957362],
-radius: 500,
-type: 'restaurant'
-}, function(err, response) {
-  if (!err) {
-    console.log(response.json.results);
-     return response.json;
-  }
+  key: 'AIzaSyDSVzX5h_PsWX6llQXnNLvGQv5yHAgdNbQ'
 });
 
+Parse.Cloud.define("geo", function(request, response) {
+	response.success("Hello world!");
+	googleMapsClient.places({
+		language: 'en',
+location: [-33.865, 151.038],
+radius: 500,
+type: 'restaurant'
+	},function(err, response) {
+	  if (!err) {
+	    console.log(response.json.results);
+  	  }
 });
