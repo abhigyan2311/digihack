400ff52ad0b44f8b99ed4620051313ec66a0f4f0
Harjot <harjot4995@gmail.com>
2018-02-16 00:11:32 +0530

    Cloud function

--- cloud.js
+++ cloud.js
@@ -1,2 +1,20 @@
 Parse.Cloud.define("geolocationCoupon", function(request, response) {
+var googleMapsClient = require('@google/maps').createClient({
+          key: 'AIzaSyDSVzX5h_PsWX6llQXnNLvGQv5yHAgdNbQ'
+        });
+
+
+
+googleMapsClient.places({
+language: 'en',
+location: [-33.8670522,151.1957362],
+radius: 500,
+type: 'restaurant'
+}, function(err, response) {
+  if (!err) {
+    console.log(response.json.results);
+     return response.json;
+  }
+});
+
 });
