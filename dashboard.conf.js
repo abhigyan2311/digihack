4eb055b9f7b80c47b3ee9ea9890b8d286d97e5b5
Abhigyan Singh <abhigyan2311@gmail.com>
2018-02-15 14:21:33 +0000

    Dashboard update

---
+++ dashboard.conf.js
@@ -0,0 +1,16 @@
+{
+  "apps": [
+    {
+       "serverURL": "http://ec2-13-127-176-156.ap-south-1.compute.amazonaws.com:1337/parse",
+      "appId": "appKey231195",
+      "masterKey": "iAmGod231195",
+      "appName": "DigiHack"
+    }
+  ],
+"users": [
+    {
+      "user":"digiadmin",
+      "pass":"iAmGod231195"
+    }
+  ]
+}
